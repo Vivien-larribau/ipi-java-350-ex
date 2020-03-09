f43dbbe6ee3c8f5fdbbabd720ab145abfa8b4e67
vivien <vivien.larribau@ipilyon.net>
2020-03-09 16:25:48 +0100

    new package repo

---
+++ src/test/java/repository/EmployeRepositoryTest.java
@@ -0,0 +1,19 @@
+package repository;
+
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class) //Junit 5
+@SpringBootTest
+public class EmployeRepositoryTest {
+
+    @Autowired
+    EmployeRepository employeRepository;
+
+
+
+    // testing //
+}
